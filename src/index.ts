1e868bcc8f517eeddc54c37bdc47b75adbb8033f
Pritesh <priteshpmantri@gmail.com>
2022-03-05 00:26:32 +0530

    graphql setup

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,16 @@
 import { createConnection } from "typeorm";
 import { getConfig } from "./config";
+import express from "express";
+import cors from "cors";
 import { Invoice, Item, Store } from "./entity";
+import { graphqlHTTP } from "express-graphql";
+import { buildSchema } from "type-graphql";
+import { HelloResolver } from "./resolver/hello.resolver";
 
 async function main() {
 	const config = getConfig();
 
-	const connection = await createConnection({
+	await createConnection({
 		type: "mysql",
 		host: config.DB_HOST,
 		username: config.DB_USER,
@@ -15,6 +20,24 @@ async function main() {
 	});
 
 	console.log("Database connected");
+
+	const app = express();
+
+	app.use(cors());
+
+	app.use(
+		"/graphql",
+		graphqlHTTP({
+			schema: await buildSchema({
+				resolvers: [HelloResolver],
+			}),
+			graphiql: true,
+		})
+	);
+
+	app.listen(5000, () => {
+		console.log("listening at http://localhost:5000");
+	});
 }
 
 main();
